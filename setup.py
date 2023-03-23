40435d6a7fec2e55bad93dd9272d89c4fd137e28
DBoyara <boyarshin.den@yandex.ru>
2023-03-23 21:07:21 +0500

    rename

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="finamTradeApi",
+    name="finam-trade-api",
     maintainer="DBoyara",
     maintainer_email="boyarshin.den@yandex.ru",
     packages=find_packages(),
-    version="1.1.1",
+    version="1.1.2",
     install_requires=["aiohttp >= 3.8.3, < 4.0.0", "pydantic >= 1.10.2, < 2.0.0"],
     python_requires=">3.7.0, <4",
     license="GNU GPL v.3.0",

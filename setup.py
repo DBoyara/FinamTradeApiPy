dc803c41d158351e75b8fc5d3395a520e17110ef
DBoyara <boyarshin.den@yandex.ru>
2025-05-09 16:02:06 +0500

    update version with new board

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     maintainer="DBoyara",
     maintainer_email="boyarshin.den@yandex.ru",
     packages=find_packages(),
-    version="3.1.1",
+    version="3.1.2",
     install_requires=["aiohttp >= 3.10.11, < 4.0.0", "pydantic >= 2.8.0, < 3.0.0"],
     python_requires=">3.11.0, <4",
     license="GNU GPL v.3.0",

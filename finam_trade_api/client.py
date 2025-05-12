71cd2cd171d9bbf04c3c7433bf3a24a6ccfa23ed
DBoyara <44875043+DBoyara@users.noreply.github.com>
2025-05-12 19:14:55 +0500

    [new api] account methods (#57)

--- finam_trade_api/client.py
+++ finam_trade_api/client.py
@@ -1,14 +1,14 @@
 from finam_trade_api.access import TokenClient
+from finam_trade_api.account import AccountClient
 from finam_trade_api.base_client.token_manager import TokenManager
 from finam_trade_api.candles import CandlesClient
 from finam_trade_api.order import OrderClient
-from finam_trade_api.portfolio import PortfolioClient
 from finam_trade_api.securities import SecurityClient
 
 
 class Client:
     def __init__(self, token_manger: TokenManager):
-        self.portfolio = PortfolioClient(token_manger)
+        self.account = AccountClient(token_manger)
         self.securities = SecurityClient(token_manger)
         self.orders = OrderClient(token_manger)
         self.access_tokens = TokenClient(token_manger)
--- tests/test_client.py
+++ tests/test_client.py
@@ -4,7 +4,7 @@ from finam_trade_api.access.access_token import TokenClient
 from finam_trade_api.candles.candles import CandlesClient
 from finam_trade_api.client import Client
 from finam_trade_api.order.order import OrderClient
-from finam_trade_api.portfolio.portfolio import PortfolioClient
+from finam_trade_api.account.account import AccountClient
 from finam_trade_api.securities.securities import SecurityClient
 from finam_trade_api.base_client.token_manager import TokenManager
 
@@ -16,7 +16,7 @@ def client():
 
 
 def test_client_initialization(client):
-    assert isinstance(client.portfolio, PortfolioClient)
+    assert isinstance(client.account, AccountClient)
     assert isinstance(client.securities, SecurityClient)
     assert isinstance(client.orders, OrderClient)
     assert isinstance(client.access_tokens, TokenClient)
